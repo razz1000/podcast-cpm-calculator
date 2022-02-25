03aec55d477739da19552170c55e35e36188e690
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-02-25 13:07:08 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -16,8 +16,10 @@ function calc() {
       }
       sleep(2000).then(() => { 
 
-    if (document.getElementById("cpm").value < 45) {
+    if (document.getElementById("cpm").value > 10 && document.getElementById("cpm").value < 45) {
         cpmResult.innerHTML = 'You are within the recommended CPM range.👍'
+    } else if (document.getElementById("cpm").value >= 0 && document.getElementById("cpm").value <= 10) {
+        cpmResult.innerHTML = 'Your price is too low. You should consider raising your price.⬆️'
     } else if (document.getElementById("cpm").value === 'NaN') {
         cpmResult.innerHTML = ''
     } else {
